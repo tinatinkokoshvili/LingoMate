2c3a07f98fcb9367bd12f01d50a549ab3c8304c9
tinatinkokoshvili <77353796+tinatinkokoshvili@users.noreply.github.com>
2021-07-20 15:26:22 -0700

    Added Checkbox, DatePicker, and Button

--- src/screens/FormPage/UserForm.js
+++ src/screens/FormPage/UserForm.js
@@ -49,6 +49,18 @@ export default function UForm() {
                 value={values.email}
                 onChange={handleInputChange}
             />
+            <Controls.Input
+                name="mobile"
+                label="Mobile"
+                value={values.mobile}
+                onChange={handleInputChange}
+            />
+            <Controls.Input
+                name="city"
+                label="City"
+                value={values.city}
+                onChange={handleInputChange}
+            />
             </Grid>
             <Grid items xs={6}>
                 <Controls.RadioGroup 
@@ -65,6 +77,29 @@ export default function UForm() {
                     onChange={handleInputChange}
                     options={employeeService.getDeaprtmentCollection()}
                 />
+                <Controls.DatePicker
+                    name="hireDate"
+                    label="Hire Date"
+                    value={values.hireDate}
+                    onChange={handleInputChange}
+                />
+                <Controls.Checkbox
+                    name="isPermanent"
+                    label="Permanent Employee"
+                    value={values.isPermanent}
+                    onChange={handleInputChange}
+                />
+                <div>
+                    <Controls.Button
+                        type="submit"
+                        text="Submit"
+                    />
+                    <Controls.Button
+                        type="reset"
+                        text="Reset"
+                        color="default"
+                    />
+                </div>
             </Grid>
         </Grid>
         </InnerForm>
