9d51d1cd30f2baea35e591e2c0f7358c08e9ef9b
Ellis Alcantara <47289830+EIIis@users.noreply.github.com>
2021-07-23 15:01:24 -0500

    Homepage finished (#10)
    * Homepage done
    
    * Finished up changes to homepage
    Please enter the commit message for your changes. Lines starting
    
    * Deleted page that would render homepage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,20 +11,23 @@ function Body() {
             <Logo />
             <div class={css(styles.rightJustify)}>
                 <div class={css(styles.buttons)}>
-                    <a class={css(styles.buttons)} href=" ">Home</a>
+                    <a class={css(styles.navText)} href=" ">Home</a>
                 </div>
                 <div class={css(styles.buttons)}>
-                    <a class={css(styles.buttons)} href="about">About</a>
+                    <a class={css(styles.navText)} href="about">About</a>
                 </div>
                 <div class={css(styles.buttons)}>
-                    <a class={css(styles.buttons)} href="contact">Contact</a>
+                    <a class={css(styles.navText)} href="contact">Contact</a>
+                </div>
+                <div class={css(styles.buttons)}>
+                    <a class={css(styles.navText)} href="contact">Login</a>
                 </div>
                 <div class={css(styles.buttons)}>
                     <Link to="/profile" class={css(styles.buttons)} href="userprofile" className="nav-links">Profile</Link>
                 </div>
                 <div class={css(styles.buttons)}>
                     <button class={css(styles.button)} onClick={() => alert('Hello, world!')}>
-                    Login
+                    Sign Up
                     </button>
                 </div>
             </div>
@@ -40,15 +43,15 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         height: 103,
+        width: '100%',
     },
     buttons: {
-        float: 'left',
         textAlign: 'center',
-        padding: '14px 16px',
         font: '17px',
         paddingRight: 50,
+        color: Color.white,
     },
-    button: { // Check how fonts work...
+    button: {
         background: Color.signupbutton,
         color: 'white',
         textAlign: 'center',
@@ -56,18 +59,24 @@ const styles = StyleSheet.create({
         height: 49,
         borderWidth: 0,
         borderRadius: 30,
-        font: '500 18px Nunito',
+        font: '18px Nunito',
         transition: 'transform 450ms',
         ':hover': {
             transform: 'scale(0.97)',
         },
     },
-    navText: { //Comeback and try to make it align... for now useless
+    navText: {
         color: 'black',
         width: 50,
         height: 25,
         textAlign: 'center',
-        font: '500 18px Nunito'
+        font: '18px Nunito',
+        padding: '14px 16px',
+        float: 'left',
+        textDecoration: 'none',
+        ':hover': {
+            color: 'white',
+        },
     },
     rightJustify: {
         justifyContent: 'flex-end',
